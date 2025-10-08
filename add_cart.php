7bf45ad2023b2f3381488a867f91ddfd630cdaca
Ronaldo_Battisti <ronaldo.battisti@hotmail.com>
2025-10-08 23:34:16 +1000

    Fixed insertion into cart, for sm reason global variables dont work in php sql request

--- add_cart.php
+++ add_cart.php
@@ -3,18 +3,20 @@
     include 'start_session_safe.php';
 
     if (isset($_SESSION['user_id'])){
+        $user_id = $_SESSION['user_id'];
         
         //add_cart.php
         if (isset($_POST['itemID'])) {
             $itemID = $_POST['itemID'];
             //echo 'user id is: ' . $_SESSION['user_id'];
-            $sql = "SELECT * FROM dbcart WHERE userID = $_SESSION[user_id]";
+            $sql = "SELECT * FROM dbcart WHERE userID = $user_id";
+            echo $user_id;
             $result = $conn->query($sql);
             if ($result && $result->num_rows > 0){
                 echo 'Already have a cart';
             } else {
-                $stmt = $conn->prepare("INSERT INTO dbcart (userID, itemID, quant) VALUES (?, ?, ?)");
-                $stmt->bind_param("iii", $_SESSION[user_id], $itemID, 1);
+                $stmt = $conn->prepare("INSERT INTO dbcart (userID, itemID, quant) VALUES (?, ?, 1)");
+                $stmt->bind_param("ii", $user_id, $itemID);
             }
 ////
             if ($stmt->execute()) {
