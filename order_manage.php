e075b6fa9a029875d74a4921144dadfca6166255
Ronaldo_Battisti <ronaldo.battisti@hotmail.com>
2025-10-24 14:15:37 +1000

    Started order_manage.php, created order view, need AJAX update

--- order_manage.php
+++ order_manage.php
@@ -5,21 +5,62 @@
     include __DIR__ . '/config/site.php';
 
     $user_id = $_SESSION['user_id'];
-    $sql = "SELECT from "
-    
+    $sql = "SELECT i.*, c.lpa_client_firstname, c.lpa_client_lastname 
+            FROM lpa_invoices i
+            JOIN lpa_clients c on i.lpa_inv_client_id = c.lpa_client_id";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
+    $hasOrder = $result->num_rows > 0;
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>View Orders</title>
+        <link rel="stylesheet" href="<?php echo BASE_URL . '/assets/css/styles.css'; ?>">
+    </head>
+    <body class="body"> 
+        <div><?php include __DIR__ . '/includes/header.php'; ?></div>
 
-</body>
+        <table border='1'>
+            <tr>
+                <th>Invoice Number</th>
+                <th>Costumer name</th>
+                <th>Date</th>
+                <th>Total value</th>
+                <th>Payment status</th>
+                <th>Order status</th>
+            </tr>
+
+            <?php if ($hasOrder) {
+                while ($row = $result->fetch_assoc()) {
+                    $inv_no = $row['lpa_inv_no'];
+                    $client_name = $row['lpa_client_firstname'] . ' ' . $row['lpa_client_lastname'];
+                    $inv_date = $row['lpa_inv_date'];
+                    $inv_amount = $row['lpa_inv_amount'];
+                    $inv_payment = $row['lpa_inv_payment_type'];
+                    $inv_status = $row['lpa_inv_status'];
+                    ?>
+                    <tr>
+                        <td><?php echo $inv_no ?></td>
+                        <td><?php echo $client_name ?></td>
+                        <td><?php echo $inv_date ?></td>
+                        <td><?php echo $inv_amount ?></td>
+                        <td><?php echo $inv_payment ?></td>
+                        <td><?php echo $inv_status ?></td>
+                    </tr>
+                    <?php
+                }
+            }
+
+            ?>
+        </table>
+
+    </body>
+    <?php include __DIR__ . '/includes/footer.html'; ?>
 </html>
