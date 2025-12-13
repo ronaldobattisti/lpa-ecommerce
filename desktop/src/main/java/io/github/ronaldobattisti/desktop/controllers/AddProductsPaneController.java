911e9af1470d959c71fa31f3f8b76fc844dce09f
Ronaldo_Battisti <ronaldo.battisti@hotmail.com>
2025-12-13 15:03:22 +1000

    Mino changes on the controller, Ill start to study for a tec

--- desktop/src/main/java/io/github/ronaldobattisti/desktop/controllers/AddProductsPaneController.java
+++ desktop/src/main/java/io/github/ronaldobattisti/desktop/controllers/AddProductsPaneController.java
@@ -2,19 +2,13 @@ package io.github.ronaldobattisti.desktop.controllers;
 
 import com.gluonhq.charm.glisten.control.DropdownButton;
 import io.github.ronaldobattisti.desktop.api.UploadApiClient;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
-
 import java.io.File;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 public class AddProductsPaneController {
     @FXML private Node root;
@@ -24,7 +18,6 @@ public class AddProductsPaneController {
     @FXML private TextField productPrice;
     @FXML private DropdownButton productCategory;
     @FXML private ImageView previewImage;
-    private File selectedFile;
 
     @FXML
     private void chooseImage() {
@@ -38,7 +31,7 @@ public class AddProductsPaneController {
                 )
         );
 
-        selectedFile = chooser.showOpenDialog(null);
+        File selectedFile = chooser.showOpenDialog(null);
 
         if (selectedFile != null) {
             Image img = new Image(selectedFile.toURI().toString());
